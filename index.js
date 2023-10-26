c2a9216f4a4b4886e2977a627450a37e3cd736d9
Yasmin <alunoyasminsilva07@gmail.com>
2023-10-26 14:59:43 -0300

    recebendo parâmetros na rota

--- index.js
+++ index.js
@@ -5,8 +5,19 @@ const app = express()
 
 const caminhoBase = path.join(__dirname, "templates")
 
+// https://localhost:3000/usuarios/20
+app.get('/usuarios/:id', (requisicao, resposta) => {
+    const id = requisicao.params.id
+    console.log(`Acessando dados do usuário ${id}`)
+    resposta.sendFile(`${caminhoBase}/usuarios.html`)
+})
+
+// : criar propriedade dentro dos parâmetros
+// requisicao.params.(nome da propriedade) depende do nome da propriedade
+
 // dirname = diretório, nome da pasta
 
+// https://localhost:3000
 app.get('/', (requisicao, resposta) => {
     resposta.sendFile(`${caminhoBase}/index.html`)
 })

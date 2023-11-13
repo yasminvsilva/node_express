23e3c8078f3af19fad9de45f18925ccfdc7cdb32
Yasmin <alunoyasminsilva07@gmail.com>
2023-11-13 13:23:58 -0300

    recebendo informações do formulário no corpo da requisição

--- index.js
+++ index.js
@@ -5,6 +5,24 @@ const app = express()
 
 const caminhoBase = path.join(__dirname, "templates")
 
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(express.json())
+
+app.post('/cadastrar/salvar', (requisicao, resposta) => {
+    const nome = requisicao.body.nome
+    const email = requisicao.body.email
+    const senha = requisicao.body.senha
+
+    console.log(`O email do usuário é: ${email}`)
+})
+
+app.get('/cadastrar', (requisicao, resposta) => {
+resposta.sendFile(`${caminhoBase}/cadastro.html`)
+})
+
 // https://localhost:3000/usuarios/20
 app.get('/usuarios/:id', (requisicao, resposta) => {
     const id = requisicao.params.id
